08f00a8abd5291aacea6416a5a21cf8cdc637b92
Fdhvdu <fdhvducipher@gmail.com>
2016-01-13 11:21:59 +0800

    fix variable name

--- header/tool/show.cpp
+++ header/tool/show.cpp
@@ -4,13 +4,13 @@
 namespace nTool
 {
 	template<class InIter,class T,class UnaryOp>
-	void show(InIter begin,InIter end,std::ostream &os,const T &delim,const UnaryOp pred)
+	void show(InIter begin,InIter end,std::ostream &os,const T &delim,const UnaryOp op)
 	{
 		using namespace std;
 		if(begin!=end)
 		{
-			os<<pred(*begin);
-			for_each(++begin,end,[&,pred](const typename iterator_traits<InIter>::value_type &val){os<<delim<<op(val);});
+			os<<op(*begin);
+			for_each(++begin,end,[&,op](const typename iterator_traits<InIter>::value_type &val){os<<delim<<op(val);});
 			os<<endl;
 		}
 	}
