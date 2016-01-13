219edfb801da5a19fc9686d64a0c3ca301478108
Fdhvdu <fdhvducipher@gmail.com>
2016-01-13 11:29:31 +0800

    update and add multiply_tutorial

--- tutorial/algorithm.cpp
+++ tutorial/algorithm.cpp
@@ -1,19 +1,38 @@
 #include<algorithm>
+#include<iterator>
 #include<vector>
 #include"../header/algorithm/algorithm.h"
 #include"../header/tool/show.h"
-using namespace std;
+
+void multiply_tutorial()
+{
+	using namespace std;
+
+	vector<int> vec{1,2,3};
+	vector<int> vec2{6,5,4};
+	vector<int> vec3;
+	nAlgorithm::multiply(begin(vec),end(vec),begin(vec2),end(vec2),back_inserter(vec3),[](const int lhs,const int rhs){return lhs+rhs;});
+	nTool::show(vec3);
+}
+
+void unique_without_sort_tutorial()
+{
+	using namespace std;
+
+	vector<int> vec{1,2,1,3,4};
+	nTool::show(begin(vec),unique(begin(vec),end(vec)));
+	nTool::show(begin(vec),nAlgorithm::unique_without_sort(begin(vec),end(vec)));
+
+	vector<int> vec2{1,2,1,3,4};
+	nTool::show(begin(vec),nAlgorithm::unique_without_sort(begin(vec),end(vec),[](const auto &lhs,const auto &rhs){return lhs%3==rhs%3;}));
+
+	//nAlgorithm::unique_without_sort_thr is a thread version, the parameter N means
+	//how large distance between begin and end
+	//will create a thread to call unique_without_sort
+}
 
 int main()
 {
-	vector<int> vec{1,2,1,3,4};
-	nTool::show(vec.begin(),unique(vec.begin(),vec.end()));
-	nTool::show(vec.begin(),nAlgorithm::unique_without_sort(vec.begin(),vec.end()));
-
-	vector<int> vec2{1,2,1,3,4};
-	nTool::show(vec.begin(),nAlgorithm::unique_without_sort(vec.begin(),vec.end(),[](const auto &lhs,const auto &rhs){return lhs%3==rhs%3;}));
-
-	//nAlgorithm::unique_without_sort_thr is thread version, the parameter N means
-	//how large distance between begin and end
-	//will create a thread to call unique_without_sort
+	multiply_tutorial();
+	unique_without_sort_tutorial();
 }
