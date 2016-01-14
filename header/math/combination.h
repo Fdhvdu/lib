ffeeedf02b9ad6cb1ec21a0c9ebecabf26df5f0e
Fdhvdu <fdhvducipher@gmail.com>
2016-01-14 12:09:06 +0800

    add comment

--- header/math/combination.h
+++ header/math/combination.h
@@ -8,6 +8,7 @@ namespace nMath
 {
 	template<class InIter>
 	std::vector<std::vector<typename std::iterator_traits<InIter>::value_type>> combination(InIter,InIter,std::size_t);
+	//require iterator_traits<InIter>::value_type copyable
 }
 
 #include"combination.cpp"
