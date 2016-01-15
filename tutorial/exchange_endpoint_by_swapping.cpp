3e6d6143153b501816fa265d00beb3d993eb42df
Fdhvdu <fdhvducipher@gmail.com>
2016-01-15 11:06:06 +0800

    make it simple

--- tutorial/exchange_endpoint_by_swapping.cpp
+++ tutorial/exchange_endpoint_by_swapping.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 	using namespace std;
-	vector<int> vec{1,2,3,4,5};
+	vector<int> vec{1,2,3,4};
 	for(const auto &val:nAlgorithm::exchange_endpoint_by_swapping(begin(vec),end(vec),[](const int lhs,const int rhs){return pair<int,int>{lhs,rhs};}))
 		nTool::show(begin(val),end(val),cout,"  ",[](const pair<int,int> &val){return to_string(val.first)+','+to_string(val.second);});
 }
