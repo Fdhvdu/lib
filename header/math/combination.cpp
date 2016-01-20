79a291551e909235d60fd440ed489f1d555c746b
Fdhvdu <fdhvducipher@gmail.com>
2016-01-20 11:43:22 +0800

    move in C, remove useless header, update C (begin, end, const auto)

--- header/math/combination.cpp
+++ header/math/combination.cpp
@@ -1,10 +1,39 @@
 #include"combination.h"
+#include<algorithm>
 #include<functional>	//function
+#include<numeric>	//iota
 #include<type_traits>	//make_unsigned
-#include"math.h"
 
 namespace nMath
 {
+	template<class T>
+	T C(const T n,const T k)
+	{
+		using namespace std;
+		//To speed up, I do not call factorial.
+		T start{max(n-k,k)};
+		vector<T> numerator(n-start),divisor(n-start);
+		iota(begin(numerator),end(numerator),start+1);
+		iota(begin(divisor),end(divisor),1);
+		T product{1};
+		for(auto &val:numerator)
+		{
+			for(auto &val2:numerator)
+			{
+				const T temp{gcd(val,val2)};
+				if(temp!=1)
+				{
+					val/=temp;
+					val2/=temp;
+				}
+			}
+			product*=val;
+		}
+		for(const auto val:divisor)
+			product/=val;
+		return product;
+	}
+
 	template<class InIter>
 	std::vector<std::vector<typename std::iterator_traits<InIter>::value_type>> combination(InIter gbegin,const InIter gend,const std::size_t take_count)
 	{
--- header/math/combination.h
+++ header/math/combination.h
@@ -6,6 +6,9 @@
 
 namespace nMath
 {
+	template<class T>
+	T C(T,T);
+
 	template<class InIter>
 	std::vector<std::vector<typename std::iterator_traits<InIter>::value_type>> combination(InIter,InIter,std::size_t);
 	//require iterator_traits<InIter>::value_type copyable
