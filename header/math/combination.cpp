0ee6d5e23df334bfe9d4b90acce3f19e7560319b
Fdhvdu <fdhvducipher@gmail.com>
2016-01-20 11:52:54 +0800

    update combination (begin, end, static_cast)

--- header/math/combination.cpp
+++ header/math/combination.cpp
@@ -47,14 +47,14 @@ namespace nMath
 					combination_(next(begin),next(end),vec,level+1);
 				++begin;
 				if(begin!=end)
-					vec.emplace_back(vec.back().begin(),next(vec.back().begin(),level));
+					vec.emplace_back(begin(vec.back()),next(begin(vec.back()),level));
 			}
 		}};
 		const auto dis{make_unsigned<ptrdiff_t>::type(distance(gbegin,gend))};
 		if(!dis||dis<take_count)
 			return {};
 		Vec vec(1);
-		vec.reserve(C<make_unsigned<ptrdiff_t>::type>(dis,take_count));
+		vec.reserve(C(dis,static_cast<make_unsigned<ptrdiff_t>::type>(take_count)));
 		combination_(gbegin,prev(gend,take_count-1),vec,0);
 		return vec;
 	}
