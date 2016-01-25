56f31df8566faf12641a94aa55ac4de9c25081dc
Fdhvdu <fdhvducipher@gmail.com>
2016-01-25 15:46:19 +0800

    add how to use calc_time

--- tutorial/chrono_timer.cpp
+++ tutorial/chrono_timer.cpp
@@ -12,17 +12,19 @@ void print_timer(const nTool::CChrono_timer &timer)
 	cout<<timer.duration_nanoseconds()%1000<<endl;
 }
 
+void iterative_2000000000()
+{
+	for(volatile auto i{2000000000+1};--i;)
+		;
+}
+
 int main()
 {
 	nTool::CChrono_timer timer;
 	timer.start();
-	for(auto i{1000000000};--i;)
+	for(volatile auto i{1000000000+1};--i;)
 		;
 	timer.stop();
 	print_timer(timer);
-	timer.start();
-	for(auto i{2000000000};--i;)
-		;
-	timer.stop();
-	print_timer(timer);
+	print_timer(nTool::calc_time(iterative_2000000000));
 }
