f497abff79b431bfbbd3b65809c6d284eb4bf4d0
Fdhvdu <fdhvducipher@gmail.com>
2016-02-15 11:51:09 +0800

    remove class template, move constructor and clear

--- header/tool/CScopeGuard.hpp
+++ header/tool/CScopeGuard.hpp
@@ -5,30 +5,18 @@
 
 namespace nTool
 {
-	template<class Func_t>
 	class CScopeGuard	//call function when exiting scope
 	{
-		std::function<Func_t> func_;
-		bool exec_;
+		const std::function<void()> func_;
 	public:
 		template<class Func,class ... Args>
 		explicit CScopeGuard(Func &&func,Args &&... args)
-			:func_{std::bind(std::forward<Func>(func),std::forward<Args>(args)...)},exec_{true}{}
+			:func_{std::bind(std::forward<Func>(func),std::forward<Args>(args)...)}{}
 		CScopeGuard(const CScopeGuard &)=delete;
-		CScopeGuard(CScopeGuard &&rVal) noexcept
-			:func_{std::move(rVal.func_)},exec_{rVal.exec_}
-		{
-			rVal.exec_=false;
-		}
-		void clear() noexcept	//do not call func_
-		{
-			exec_=false;
-		}
 		CScopeGuard& operator=(const CScopeGuard &)=delete;
 		~CScopeGuard()
 		{
-			if(exec_)
-				func_();
+			func_();
 		}
 	};
 }
