38bedb59454f33e568164f259e9157b41852cf3a
Fdhvdu <fdhvducipher@gmail.com>
2016-03-02 19:47:32 +0800

    replace vector with deque

--- header/algorithm/exchange_endpoint_by_swapping.hpp
+++ header/algorithm/exchange_endpoint_by_swapping.hpp
@@ -2,6 +2,7 @@
 #define EXCHANGE_ENDPOINT_BY_SWAPPING
 #include<algorithm>
 #include<functional>
+#include<deque>
 #include<iterator>	//cbegin, cend
 #include<type_traits>	//result_of_t
 #include<utility>
@@ -14,50 +15,58 @@ namespace nAlgorithm
 	std::vector<std::vector<std::result_of_t<BinaryOp(typename std::iterator_traits<BidIter>::value_type,typename std::iterator_traits<BidIter>::value_type)>>> exchange_endpoint_by_swapping(const BidIter begin,const BidIter end,const BinaryOp op)
 	{
 		using namespace std;
-		using Vec=vector<vector<pair<const BidIter,const BidIter>>>;
-		function<void(BidIter,BidIter,Vec &,size_t)> exchange_endpoint_by_swapping_;
-		function<void(BidIter,BidIter,Vec &,size_t)> to_right_{[&](BidIter to_right,const BidIter to_left,Vec &vec,const size_t level){
-			vec.back().emplace_back(to_right,next(to_right));
+		using Temp_Container=deque<vector<pair<const BidIter,const BidIter>>>;
+		const auto dist{static_cast<size_t>(distance(begin,end))};
+		if(dist<2)
+			return {};
+		function<void(BidIter,BidIter,Temp_Container &,size_t)> exchange_endpoint_by_swapping_;
+		const auto reserve{[dist](Temp_Container::value_type &container){
+			container.reserve(2*dist-3);
+		}};
+		const function<void(BidIter,BidIter,Temp_Container &,size_t)> to_right_{[&](BidIter to_right,const BidIter to_left,Temp_Container &container,const size_t level){
+			container.back().emplace_back(to_right,next(to_right));
 			advance(to_right,1);
 			if(to_right==to_left)
-				exchange_endpoint_by_swapping_(to_right,prev(to_left),vec,level+1);
+				exchange_endpoint_by_swapping_(to_right,prev(to_left),container,level+1);
 			else
-				exchange_endpoint_by_swapping_(to_right,to_left,vec,level+1);
+				exchange_endpoint_by_swapping_(to_right,to_left,container,level+1);
 		}};
-		function<void(BidIter,BidIter,Vec &,size_t)> to_left_{[&](const BidIter to_right,BidIter to_left,Vec &vec,const size_t level){
-			vec.back().emplace_back(prev(to_left),to_left);
+		const function<void(BidIter,BidIter,Temp_Container &,size_t)> to_left_{[&](const BidIter to_right,BidIter to_left,Temp_Container &container,const size_t level){
+			container.back().emplace_back(prev(to_left),to_left);
 			advance(to_left,-1);
 			if(to_right==to_left)
-				exchange_endpoint_by_swapping_(next(to_right),to_left,vec,level+1);
+				exchange_endpoint_by_swapping_(next(to_right),to_left,container,level+1);
 			else
-				exchange_endpoint_by_swapping_(to_right,to_left,vec,level+1);
+				exchange_endpoint_by_swapping_(to_right,to_left,container,level+1);
 		}};
-		exchange_endpoint_by_swapping_=[&,begin,end](const BidIter to_right,const BidIter to_left,Vec &vec,const size_t level){
+		exchange_endpoint_by_swapping_=[&,begin,dist,end](const BidIter to_right,const BidIter to_left,Temp_Container &container,const size_t level){
 			if(next(to_right)==to_left)
-				to_right_(to_right,to_left,vec,level);
+				to_right_(to_right,to_left,container,level);
 			else
 			{
 				bool copy{false};
 				if(to_right!=prev(end))
 				{
-					to_right_(to_right,to_left,vec,level);
+					to_right_(to_right,to_left,container,level);
 					copy=true;
 				}
 				if(to_left!=begin)
 				{
 					if(copy)
-						vec.emplace_back(std::cbegin(vec.back()),next(std::cbegin(vec.back()),level));
-					to_left_(to_right,to_left,vec,level);
+					{
+						container.emplace_back(std::cbegin(container.back()),next(std::cbegin(container.back()),level));
+						reserve(container.back());
+					}
+					to_left_(to_right,to_left,container,level);
 				}
 			}
 		};
-		if(static_cast<size_t>(distance(begin,end))<2)
-			return {};
-		Vec vec(1);	//not {}
-		exchange_endpoint_by_swapping_(begin,prev(end),vec,0);
+		Temp_Container container(1);	//not {}
+		reserve(container.back());
+		exchange_endpoint_by_swapping_(begin,prev(end),container,0);
 		result_of_t<decltype(exchange_endpoint_by_swapping<BidIter,BinaryOp>)&(BidIter,BidIter,BinaryOp)> result;
-		result.reserve(vec.size());
-		for(const auto &val:vec)
+		result.reserve(container.size());
+		for(const auto &val:container)
 		{
 			result.emplace_back();
 			result.back().reserve(val.size());
