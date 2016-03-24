9a19885644813b95376310c332ad8f328b327e66
Fdhvdu <fdhvducipher@gmail.com>
2016-03-24 16:17:45 +0800

    add CThread_forward_list (require C++17)

--- header/thread/CThread_forward_list.hpp
+++ header/thread/CThread_forward_list.hpp
@@ -5,6 +5,12 @@
 #include<memory>	//allocator
 #include<mutex>
 #include<utility>	//forward, move_if_noexcept
+//#include<atomic>
+//#include<condition_variable>
+//#include<memory>	//shared_ptr
+//#include<mutex>
+//#include<shared_mutex>
+//#include<utility>	//forward, move, swap
 
 namespace nThread
 {
@@ -57,6 +63,108 @@ namespace nThread
 			return temp;
 		}
 	};
+
+	//template<class T>
+	//class CThread_forward_list
+	//{
+	//public:
+	//	using value_type=T;
+	//private:
+	//	struct Node
+	//	{
+	//		value_type data;
+	//		std::shared_ptr<Node> next;
+	//		template<class shared_ptrFwdRef,class ... Args>
+	//		Node(shared_ptrFwdRef &&next_,Args &&...args)
+	//			:data{std::forward<decltype(args)>(args)...},next{std::forward<decltype(next_)>(next_)}{}
+	//		~Node()
+	//		{
+	//			while(next.use_count()==1)
+	//			{
+	//				std::shared_ptr<Node> p{std::move(next->next)};
+	//				next.reset();
+	//				next=std::move(p);
+	//			}
+	//		}
+	//	};
+	//	std::shared_ptr<Node> begin_;
+	//	std::condition_variable cv_;
+	//	std::shared_mutex remove_mut_;
+	//	std::mutex wait_mut_;
+	//public:
+	//	CThread_forward_list()=default;
+	//	CThread_forward_list(const CThread_forward_list &)=delete;
+	//	template<class ... Args>
+	//	void emplace_front(Args &&...args)
+	//	{
+	//		const std::shared_ptr<Node> node{std::make_shared<Node>(std::atomic_load_explicit(&begin_,std::memory_order_relaxed),std::forward<decltype(args)>(args)...)};
+	//		std::shared_lock<std::shared_mutex> lock{remove_mut_};
+	//		while(!std::atomic_compare_exchange_weak_explicit(&begin_,&node->next,node,std::memory_order_release,std::memory_order_relaxed))
+	//			;
+	//		cv_.notify_one();
+	//	}
+	//	//1. do not call emplace_not_ts with greater than or equal to 2 threads at same time
+	//	//2. do not call CAtomic_stack::pop_front, CAtomic_stack::remove, CAtomic_stack::remove_if or CAtomic_stack::wait_and_pop_front at same time
+	//	template<class ... Args>
+	//	inline void emplace_not_ts(Args &&...args)
+	//	{
+	//		begin_=std::make_shared<Node>(begin_,std::forward<decltype(args)>(args)...);
+	//	}
+	//	inline bool empty() const noexcept
+	//	{
+	//		return !begin_.operator bool();
+	//	}
+	//	//if constructor or assignment operator you use here is not noexcept, it may not be exception safety
+	//	value_type pop_front()
+	//	{
+	//		std::shared_ptr<Node> node{std::atomic_load_explicit(&begin_,std::memory_order_relaxed)};
+	//		std::shared_lock<std::shared_mutex> lock{remove_mut_};
+	//		while(!std::atomic_compare_exchange_weak_explicit(&begin_,&node,node->next,std::memory_order_acquire,std::memory_order_relaxed))
+	//			;
+	//		return std::move(node->data);
+	//	}
+	//	inline void remove(const value_type &remove_val)
+	//	{
+	//		remove_if([&](const auto &val) noexcept{return val==remove_val;});
+	//	}
+	//	//will block CAtomic_stack::emplace_front, CAtomic_stack::pop_front and CAtomic_stack::wait_and_pop_front
+	//	template<class UnaryPred>
+	//	void remove_if(const UnaryPred pred)
+	//	{
+	//		std::lock<std::shared_mutex> lock{remove_mut_};
+	//		for(std::shared_ptr<Node> bef{begin_},iter{bef};iter;)
+	//			if(pred(iter->data))
+	//				if(begin_==iter)
+	//				{
+	//					iter=iter->next;
+	//					begin_=bef=iter;
+	//				}
+	//				else
+	//				{
+	//					std::swap(bef->next,iter->next);
+	//					iter=bef->next;
+	//				}
+	//			else
+	//			{
+	//				bef=iter;
+	//				iter=iter->next;
+	//			}
+	//	}
+	//	//1. if constructor or assignment operator you use here is not noexcept, it may not be exception safety
+	//	//2. will block wait_and_pop_front
+	//	value_type wait_and_pop_front()
+	//	{
+	//		std::unique_lock<std::mutex> lock{wait_mut_};
+	//		cv_.wait(lock,[this]() noexcept{return !empty();});
+	//		//1. if move constructor is noexcept, it is exception safety
+	//		//2. if move constructor is not noexcept and copy constructor exists, it is exception safety
+	//		//3. if move constructor is not noexcept and copy constructor does not exist, it may not be exception safety
+	//		const auto temp{pop_front()};
+	//		lock.unlock();
+	//		return temp;
+	//	}
+	//	CThread_forward_list& operator=(const CThread_forward_list &)=delete;
+	//};
 }
 
 #endif
