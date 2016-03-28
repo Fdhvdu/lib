1e35bd25fcf89a0ccf493d7b5eb2b249addb3a4d
Fdhvdu <fdhvducipher@gmail.com>
2016-03-28 19:39:27 +0800

    fix CNode

--- header/thread/CAtomic_stack.hpp
+++ header/thread/CAtomic_stack.hpp
@@ -11,24 +11,38 @@ namespace nThread
 	class CAtomic_stack
 	{
 	public:
+		using allocator_type=std::allocator<T>;
 		using value_type=T;
 	private:
 		struct Node
 		{
-			value_type data;
+			static allocator_type alloc;
+			typename std::allocator<value_type>::pointer data;
 			std::shared_ptr<Node> next;
+			Node()
+				:data{alloc.allocate(1)}{}
 			template<class ... Args>
-			Node(Args &&...args) noexcept(std::is_nothrow_constructible<T,Args...>::value)
-				:data{std::forward<decltype(args)>(args)...}{}
+			Node(Args &&...args)
+				:Node{}
+			{
+				alloc.construct(data,std::forward<decltype(args)>(args)...);
+			}
 			template<class shared_ptrFwdRef,class ... Args,class=std::enable_if_t<std::is_same<std::remove_cv_t<std::remove_reference_t<shared_ptrFwdRef>>,std::shared_ptr<Node>>::value>>
-			Node(shared_ptrFwdRef &&next_,Args &&...args) noexcept(std::is_nothrow_constructible<T,Args...>::value)
-				:data{std::forward<decltype(args)>(args)...},next{std::forward<decltype(next_)>(next_)}{}
+			Node(shared_ptrFwdRef &&next_,Args &&...args)
+				:data{alloc.allocate(1)},next{std::forward<decltype(next_)>(next_)}
+			{
+				alloc.construct(data,std::forward<decltype(args)>(args)...);
+			}
 			Node(const Node &)=delete;
 			Node& operator=(const Node &)=delete;
 			~Node()
 			{
+				alloc.destroy(data);
+				alloc.deallocate(data,1);
 				while(next.use_count()==1)
 				{
+					alloc.destroy(next->data);
+					alloc.deallocate(next->data,1);
 					std::shared_ptr<Node> p{std::move(next->next)};
 					next.reset();
 					next=std::move(p);
@@ -45,21 +59,13 @@ namespace nThread
 	public:
 		class CNode
 		{
-			friend void CAtomic_stack<T>::emplace_CNode(CNode &&) noexcept;
+			friend CAtomic_stack<T>;
 			std::shared_ptr<Node> p_;
 		public:
-			template<class Arg,class=std::enable_if_t<!std::is_base_of<std::remove_reference_t<Arg>,CNode>::value>>
-			CNode(Arg &&arg)
-				:p_{std::make_shared<Node>(std::forward<decltype(arg)>(arg))}{}
-			template<class ... Args,class=std::enable_if_t<(sizeof...(Args)!=1)>>
-			CNode(Args &&...args)
-				:p_{std::make_shared<Node>(std::forward<decltype(args)>(args)...)}{}
+			CNode()
+				:p_{std::make_shared<Node>()}{}
 			CNode(const CNode &)=delete;
 			CNode(CNode &&)=default;
-			value_type& get_data()
-			{
-				return p_->data;
-			}
 			CNode& operator=(const CNode &)=delete;
 		};
 		CAtomic_stack()=default;
@@ -69,8 +75,10 @@ namespace nThread
 		{
 			emplace_shared_ptr_(std::make_shared<Node>(std::forward<decltype(args)>(args)...));
 		}
-		inline void emplace_CNode(CNode &&val) noexcept
+		template<class ... Args>
+		void emplace(CNode &&val,Args &&...args) noexcept(std::is_nothrow_constructible<T,Args...>::value)
 		{
+			Node::alloc.construct(val.p_->data,std::forward<decltype(args)>(args)...);
 			emplace_shared_ptr_(std::move(val.p_));
 		}
 		//1. do not call emplace_not_ts with greater than or equal to 2 threads at same time
@@ -94,10 +102,13 @@ namespace nThread
 			std::shared_ptr<Node> node{std::atomic_load_explicit(&begin_,std::memory_order_relaxed)};
 			while(!std::atomic_compare_exchange_weak_explicit(&begin_,&node,node->next,std::memory_order_acquire,std::memory_order_relaxed))
 				;
-			return std::move(node->data);
+			return std::move(*node->data);
 		}
 		CAtomic_stack& operator=(const CAtomic_stack &)=delete;
 	};
+
+	template<class T>
+	typename CAtomic_stack<T>::allocator_type CAtomic_stack<T>::Node::alloc;
 }
 
 #endif
