8adf6400f402c97549ac8ed07990e878337f4cff
Fdhvdu <fdhvducipher@gmail.com>
2016-03-28 20:44:09 +0800

    update

--- header/thread/CThread_forward_list.hpp
+++ header/thread/CThread_forward_list.hpp
@@ -68,22 +68,38 @@ namespace nThread
 	//class CThread_forward_list
 	//{
 	//public:
+	//	using allocator_type=std::allocator<T>;
 	//	using value_type=T;
 	//private:
 	//	struct Node
 	//	{
-	//		value_type data;
+	//		static allocator_type alloc;
+	//		typename std::allocator<value_type>::pointer data;
 	//		std::shared_ptr<Node> next;
+	//		Node()
+	//			:data{alloc.allocate(1)}{}
 	//		template<class ... Args>
-	//		Node(Args &&...args) noexcept(std::is_nothrow_constructible<T,Args...>::value)
-	//			:data{std::forward<decltype(args)>(args)...}{}
+	//		Node(Args &&...args)
+	//			:Node{}
+	//		{
+	//			alloc.construct(data,std::forward<decltype(args)>(args)...);
+	//		}
 	//		template<class shared_ptrFwdRef,class ... Args,class=std::enable_if_t<std::is_same<std::remove_cv_t<std::remove_reference_t<shared_ptrFwdRef>>,std::shared_ptr<Node>>::value>>
-	//		Node(shared_ptrFwdRef &&next_,Args &&...args) noexcept(std::is_nothrow_constructible<T,Args...>::value)
-	//			:data{std::forward<decltype(args)>(args)...},next{std::forward<decltype(next_)>(next_)}{}
+	//		Node(shared_ptrFwdRef &&next_,Args &&...args)
+	//			:data{alloc.allocate(1)},next{std::forward<decltype(next_)>(next_)}
+	//		{
+	//			alloc.construct(data,std::forward<decltype(args)>(args)...);
+	//		}
+	//		Node(const Node &)=delete;
+	//		Node& operator=(const Node &)=delete;
 	//		~Node()
 	//		{
+	//			alloc.destroy(data);
+	//			alloc.deallocate(data,1);
 	//			while(next.use_count()==1)
 	//			{
+	//				alloc.destroy(next->data);
+	//				alloc.deallocate(next->data,1);
 	//				std::shared_ptr<Node> p{std::move(next->next)};
 	//				next.reset();
 	//				next=std::move(p);
@@ -92,51 +108,54 @@ namespace nThread
 	//	};
 	//	std::shared_ptr<Node> begin_;
 	//	std::condition_variable cv_;
-	//	std::shared_mutex remove_mut_;
+	//	std::mutex remove_mut_;
+	//	//std::shared_mutex remove_mut_;
 	//	std::mutex wait_mut_;
 	//	void emplace_front_(std::shared_ptr<Node> &&val) noexcept
 	//	{
+	//		val->next=std::atomic_load_explicit(&begin_,std::memory_order_relaxed);
 	//		while(!std::atomic_compare_exchange_weak_explicit(&begin_,&val->next,val,std::memory_order_release,std::memory_order_relaxed))
 	//			;
+	//	}
+	//	void notify_()
+	//	{
+	//		std::lock_guard<std::mutex> lock{wait_mut_};
+	//		cv_.notify_one();
 	//	}
 	//public:
 	//	class CNode
 	//	{
-	//		friend void CThread_forward_list<T>::emplace_CNode_front(CNode &&val) noexcept;
+	//		friend CThread_forward_list<T>;
 	//		std::shared_ptr<Node> p_;
 	//	public:
-	//		template<class Arg,class=std::enable_if_t<!std::is_base_of<std::remove_reference_t<Arg>,CNode>::value>>
-	//		CNode(Arg &&arg)
-	//			:p_{std::make_shared<Node>(std::forward<decltype(arg)>(arg))}{}
-	//		template<class ... Args,class=std::enable_if_t<(sizeof...(Args)!=1)>>
-	//		CNode(Args &&...args)
-	//			:p_{std::make_shared<Node>(std::forward<decltype(args)>(args)...)}{}
+	//		CNode()
+	//			:p_{std::make_shared<Node>()}{}
 	//		CNode(const CNode &)=delete;
 	//		CNode(CNode &&)=default;
-	//		value_type& get_data()
-	//		{
-	//			return p_->data;
-	//		}
 	//		CNode& operator=(const CNode &)=delete;
 	//	};
 	//	CThread_forward_list()=default;
 	//	CThread_forward_list(const CThread_forward_list &)=delete;
-	//	void emplace_CNode_front(CNode &&val)
-	//	{
-	//		std::shared_lock<std::shared_mutex> lock{remove_mut_};
-	//		emplace_front_(std::move(val.p_));
-	//	}
-	//	void emplace_CNode_front_and_notify(CNode &&val)
-	//	{
-	//		emplace_CNode_front(std::move(val));
-	//		std::lock_guard<std::mutex> lock{wait_mut_};
-	//		cv_.notify_one();
-	//	}
 	//	template<class ... Args>
 	//	void emplace_front(Args &&...args)
 	//	{
-	//		std::shared_lock<std::shared_mutex> lock{remove_mut_};
+	//		std::lock_guard<std::mutex> lock{remove_mut_};
+	//		//std::shared_lock<std::shared_mutex> lock{remove_mut_};
 	//		emplace_front_(std::make_shared<Node>(std::forward<decltype(args)>(args)...));
+	//	}
+	//	template<class ... Args>
+	//	void emplace_front(CNode &&val,Args &&...args)
+	//	{
+	//		Node::alloc.construct(val.p_->data,std::forward<decltype(args)>(args)...);
+	//		std::lock_guard<std::mutex> lock{remove_mut_};
+	//		//std::shared_lock<std::shared_mutex> lock{remove_mut_};
+	//		emplace_front_(std::move(val.p_));
+	//	}
+	//	template<class ... Args>
+	//	void emplace_front_and_notify(Args &&...args)
+	//	{
+	//		emplace_front(std::forward<decltype(args)>(args)...);
+	//		notify_();
 	//	}
 	//	//1. do not call emplace_not_ts with greater than or equal to 2 threads at same time
 	//	//2. do not call CAtomic_stack::pop_front, CAtomic_stack::remove, CAtomic_stack::remove_if or CAtomic_stack::wait_and_pop_front at same time
@@ -145,13 +164,6 @@ namespace nThread
 	//	{
 	//		begin_=std::make_shared<Node>(begin_,std::forward<decltype(args)>(args)...);
 	//	}
-	//	template<class ... Args>
-	//	void emplace_front_and_notify(Args &&...args)
-	//	{
-	//		emplace_front(std::forward<decltype(args)>(args)...));
-	//		std::lock_guard<std::mutex> lock{wait_mut_};
-	//		cv_.notify_one();
-	//	}
 	//	inline bool empty() const noexcept
 	//	{
 	//		return !begin_.operator bool();
@@ -159,11 +171,12 @@ namespace nThread
 	//	//if constructor or assignment operator you use here is not noexcept, it may not be exception safety
 	//	value_type pop_front()
 	//	{
-	//		std::shared_lock<std::shared_mutex> lock{remove_mut_};
+	//		std::lock_guard<std::mutex> lock{remove_mut_};
+	//		//std::shared_lock<std::shared_mutex> lock{remove_mut_};
 	//		std::shared_ptr<Node> node{std::atomic_load_explicit(&begin_,std::memory_order_relaxed)};
 	//		while(!std::atomic_compare_exchange_weak_explicit(&begin_,&node,node->next,std::memory_order_acquire,std::memory_order_relaxed))
 	//			;
-	//		return std::move(node->data);
+	//		return std::move(*node->data);
 	//	}
 	//	inline void remove(const value_type &remove_val)
 	//	{
@@ -173,9 +186,10 @@ namespace nThread
 	//	template<class UnaryPred>
 	//	void remove_if(const UnaryPred pred)
 	//	{
-	//		std::lock<std::shared_mutex> lock{remove_mut_};
+	//		std::lock_guard<std::mutex> lock{remove_mut_};
+	//		//std::lock_guard<std::shared_mutex> lock{remove_mut_};
 	//		for(std::shared_ptr<Node> bef{begin_},iter{bef};iter;)
-	//			if(pred(iter->data))
+	//			if(pred(*iter->data))
 	//				if(begin_==iter)
 	//				{
 	//					iter=iter->next;
@@ -207,6 +221,9 @@ namespace nThread
 	//	}
 	//	CThread_forward_list& operator=(const CThread_forward_list &)=delete;
 	//};
+
+	//template<class T>
+	//typename CThread_forward_list<T>::allocator_type CThread_forward_list<T>::Node::alloc;
 }
 
 #endif
