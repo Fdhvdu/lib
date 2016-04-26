c74b0619a96bf05269c6b1c5d7b3a14419c5d85e
Fdhvdu <fdhvducipher@gmail.com>
2016-04-26 15:13:56 +0800

    replace CAtomicRingBuf and CThreadRingBuf with CWait_bounded_queue

---
+++ header/thread/CWait_bounded_queue.hpp
@@ -0,0 +1,44 @@
+#ifndef CWAIT_BOUNDED_QUEUE
+#define CWAIT_BOUNDED_QUEUE
+#include"CLock_bounded_queue.hpp"
+#include"CSemaphore.hpp"
+
+namespace nThread
+{
+	template<class T>
+	class CWait_bounded_queue
+	{
+	public:
+		using size_type=typename CLock_bounded_queue<T>::size_type;
+		using value_type=T;
+	private:
+		CLock_bounded_queue<value_type> queue_;
+		CSemaphore sema_;
+	public:
+		explicit CWait_bounded_queue(const size_type size)
+			:queue_{size}{}
+		CWait_bounded_queue(const CWait_bounded_queue &)=delete;
+		template<class ... Args>
+		void emplace_and_notify(Args &&...args)
+		{
+			queue_.emplace(std::forward<decltype(args)>(args)...);
+			sema_.signal();
+		}
+		inline bool empty() const noexcept
+		{
+			return queue_.empty();
+		}
+		inline size_type size() const noexcept
+		{
+			return queue_.size();
+		}
+		value_type wait_and_pop()
+		{
+			sema_.wait();
+			return queue_.pop();
+		}
+		CWait_bounded_queue& operator=(const CWait_bounded_queue &)=delete;
+	};
+}
+
+#endif
