7ce85a6ba6b5ab9bc5883f6ca116e4276b2e814c
Fdhvdu <fdhvducipher@gmail.com>
2016-04-26 15:16:02 +0800

    update

--- header/thread/CAtomic_stack.hpp
+++ header/thread/CAtomic_stack.hpp
@@ -1,42 +1,27 @@
 #ifndef CATOMIC_STACK
 #define CATOMIC_STACK
-#include<atomic>
 #include<memory>	//shared_ptr
 #include<utility>	//forward, move
-#include"Node.hpp"
+#include"Atomic_stack.hpp"
+#include"Default_node.hpp"
 
 namespace nThread
 {
 	template<class T>
 	class CAtomic_stack
 	{
-		template<class T>
-		friend void pop_and_emplace(CAtomic_stack<T> &,CAtomic_stack<T> &) noexcept;
 	public:
 		using value_type=T;
 	private:
-		std::shared_ptr<Node<value_type>> begin_;
-		void emplace_shared_ptr_(std::shared_ptr<Node<value_type>> &&val) noexcept
-		{
-			val->next=std::atomic_load_explicit(&begin_,std::memory_order_relaxed);
-			while(!std::atomic_compare_exchange_weak_explicit(&begin_,&val->next,val,std::memory_order_release,std::memory_order_relaxed))
-				;
-		}
-		std::shared_ptr<Node<value_type>> pop_() noexcept
-		{
-			std::shared_ptr<Node<value_type>> node{std::atomic_load_explicit(&begin_,std::memory_order_relaxed)};
-			while(!std::atomic_compare_exchange_weak_explicit(&begin_,&node,node->next,std::memory_order_acquire,std::memory_order_relaxed))
-				;
-			return node;
-		}
+		Atomic_stack<value_type> stack_;
 	public:
 		class CNode
 		{
 			friend CAtomic_stack<value_type>;
-			std::shared_ptr<Node<value_type>> p_;
+			typename Atomic_stack<value_type>::shared_ptr p_;
 		public:
 			CNode()
-				:p_{std::make_shared<Node<value_type>>()}{}
+				:p_{std::make_shared<Atomic_stack<value_type>::node_type>()}{}
 			CNode(const CNode &)=delete;
 			CNode(CNode &&)=default;
 			CNode& operator=(const CNode &)=delete;
@@ -46,43 +31,35 @@ namespace nThread
 		template<class ... Args>
 		inline void emplace(Args &&...args)
 		{
-			emplace_shared_ptr_(std::make_shared<Node<value_type>>(std::forward<decltype(args)>(args)...));
+			stack_.emplace(std::make_shared<Atomic_stack<value_type>::node_type>(std::forward<decltype(args)>(args)...));
 		}
 		template<class ... Args>
 		void emplace(CNode &&val,Args &&...args) noexcept(std::is_nothrow_constructible<value_type,Args...>::value)
 		{
-			Node<value_type>::alloc.construct(val.p_->data,std::forward<decltype(args)>(args)...);
-			emplace_shared_ptr_(std::move(val.p_));
+			Atomic_stack<value_type>::node_type::alloc.construct(val.p_->data,std::forward<decltype(args)>(args)...);
+			stack_.emplace(std::move(val.p_));
 		}
-		//1. do not call emplace_not_ts with greater than or equal to 2 threads at same time
-		//2. do not call CAtomic_stack::emplace and CAtomic_stack::pop at same time
+		//do not call CAtomic_stack::emplace, emplace_not_ts or CAtomic_stack::pop at same time
 		template<class ... Args>
 		inline void emplace_not_ts(Args &&...args)
 		{
-			begin_=std::make_shared<Node<value_type>>(begin_,std::forward<decltype(args)>(args)...);
+			stack_.emplace_not_ts(std::make_shared<Atomic_stack<value_type>::node_type>(std::forward<decltype(args)>(args)...));
 		}
 		inline bool empty() const noexcept
 		{
-			return !begin_.operator bool();
+			return stack_.empty();
 		}
 		inline bool is_lock_free() const noexcept
 		{
-			return std::atomic_is_lock_free(&begin_);
+			return stack_.is_lock_free();
 		}
 		//if constructor or assignment operator you use here is not noexcept, it may not be exception safety
 		inline value_type pop() noexcept
 		{
-			return std::move(*pop_()->data);
+			return std::move(*stack_.pop()->data);
 		}
 		CAtomic_stack& operator=(const CAtomic_stack &)=delete;
 	};
-
-	template<class T>
-	void pop_and_emplace(CAtomic_stack<T> &src,CAtomic_stack<T> &des) noexcept
-	{
-		while(!src.empty())
-			des.emplace_shared_ptr_(src.pop_());
-	}
 }
 
 #endif
--- header/thread/CThread_forward_list.hpp
+++ header/thread/CThread_forward_list.hpp
@@ -1,11 +1,12 @@
 #ifndef CTHREAD_FORWARD_LIST
 #define CTHREAD_FORWARD_LIST
 #include<atomic>
-#include<condition_variable>
 #include<memory>	//shared_ptr
 #include<mutex>
+//#include<shared_mutex>
 #include<utility>	//forward, move, swap
-#include"Node.hpp"
+#include"Atomic_stack.hpp"
+#include"Default_node.hpp"
 
 namespace nThread
 {
@@ -15,40 +16,25 @@ namespace nThread
 	public:
 		using value_type=T;
 	private:
-		std::shared_ptr<Node<value_type>> begin_;
-		std::condition_variable cv_;
-		std::mutex wait_mut_;
-		void acquire_lock_and_emplace_front_(std::shared_ptr<Node<value_type>> &&val)
+		using node_type=typename Atomic_stack<value_type>::node_type;
+		using shared_ptr=typename Atomic_stack<value_type>::shared_ptr;
+		Atomic_stack<value_type> stack_;
+		std::mutex mut_;
+		//std::shared_mutex mut_;
+		void acquire_lock_and_emplace_front_(shared_ptr &&val)
 		{
-			std::lock_guard<std::mutex> lock{wait_mut_};
-			emplace_front_(std::move(val));
-		}
-		void emplace_front_(std::shared_ptr<Node<value_type>> &&val) noexcept
-		{
-			val->next=std::atomic_load_explicit(&begin_,std::memory_order_relaxed);
-			while(!std::atomic_compare_exchange_weak_explicit(&begin_,&val->next,val,std::memory_order_release,std::memory_order_relaxed))
-				;
-		}
-		void notify_()
-		{
-			std::lock_guard<std::mutex> lock{wait_mut_};
-			cv_.notify_one();
-		}
-		value_type pop_front_() noexcept
-		{
-			std::shared_ptr<Node<value_type>> node{std::atomic_load_explicit(&begin_,std::memory_order_relaxed)};
-			while(!std::atomic_compare_exchange_weak_explicit(&begin_,&node,node->next,std::memory_order_acquire,std::memory_order_relaxed))
-				;
-			return std::move(*node->data);
+			std::lock_guard<std::mutex> lock{mut_};
+			//std::shared_lock<std::shared_mutex> lock{mut_};
+			stack_.emplace(std::move(val));
 		}
 	public:
 		class CNode
 		{
 			friend CThread_forward_list<value_type>;
-			std::shared_ptr<Node<value_type>> p_;
+			shared_ptr p_;
 		public:
 			CNode()
-				:p_{std::make_shared<Node<value_type>>()}{}
+				:p_{std::make_shared<node_type>()}{}
 			CNode(const CNode &)=delete;
 			CNode(CNode &&)=default;
 			CNode& operator=(const CNode &)=delete;
@@ -56,55 +42,48 @@ namespace nThread
 		CThread_forward_list()=default;
 		CThread_forward_list(const CThread_forward_list &)=delete;
 		template<class ... Args>
-		void emplace_front(Args &&...args)
+		inline void emplace_front(Args &&...args)
 		{
-			auto temp{std::make_shared<Node<value_type>>(std::forward<decltype(args)>(args)...)};
-			acquire_lock_and_emplace_front_(std::move(temp));
+			acquire_lock_and_emplace_front_(std::make_shared<node_type>(std::forward<decltype(args)>(args)...));
 		}
 		template<class ... Args>
 		void emplace_front(CNode &&val,Args &&...args)
 		{
-			Node<value_type>::alloc.construct(val.p_->data,std::forward<decltype(args)>(args)...);
+			node_type::alloc.construct(val.p_->data,std::forward<decltype(args)>(args)...);
 			acquire_lock_and_emplace_front_(std::move(val.p_));
 		}
-		template<class ... Args>
-		void emplace_front_and_notify(Args &&...args)
-		{
-			emplace_front(std::forward<decltype(args)>(args)...);
-			notify_();
-		}
-		//1. do not call emplace_not_ts with greater than or equal to 2 threads at same time
-		//2. do not call CAtomic_stack::pop_front, CAtomic_stack::remove, CAtomic_stack::remove_if or CAtomic_stack::wait_and_pop_front at same time
+		//do not call CAtomic_stack::emplace_front, emplace_front_not_ts, CAtomic_stack::pop_front, CAtomic_stack::remove or CAtomic_stack::remove_if at same time
 		template<class ... Args>
 		inline void emplace_front_not_ts(Args &&...args)
 		{
-			begin_=std::make_shared<Node<value_type>>(begin_,std::forward<decltype(args)>(args)...);
+			stack_.emplace_not_ts(std::make_shared<node_type>(std::forward<decltype(args)>(args)...));
 		}
 		inline bool empty() const noexcept
 		{
-			return !begin_.operator bool();
+			return stack_.empty();
 		}
-		//if constructor or assignment operator you use here is not noexcept, it may not be exception safety
 		value_type pop_front()
 		{
-			std::lock_guard<std::mutex> lock{wait_mut_};
-			return pop_front_();
+			std::lock_guard<std::mutex> lock{mut_};
+			//std::shared_lock<std::shared_mutex> lock{mut_};
+			return *stack_.pop()->data;
 		}
 		inline void remove(const value_type &remove_val)
 		{
 			remove_if([&](const auto &val) noexcept{return val==remove_val;});
 		}
-		//will block CAtomic_stack::emplace_front, CAtomic_stack::pop_front and CAtomic_stack::wait_and_pop_front
+		//will block CAtomic_stack::emplace_front and CAtomic_stack::pop_front
 		template<class UnaryPred>
 		void remove_if(const UnaryPred pred)
 		{
-			std::lock_guard<std::mutex> lock{wait_mut_};
-			for(std::shared_ptr<Node<value_type>> bef{begin_},iter{bef};iter;)
+			std::lock_guard<std::mutex> lock{mut_};
+			//std::lock_guard<std::shared_mutex> lock{mut_};
+			for(shared_ptr bef{stack_.begin},iter{bef};iter;)
 				if(pred(*iter->data))
-					if(begin_==iter)
+					if(stack_.begin==iter)
 					{
 						iter=iter->next;
-						begin_=bef=iter;
+						stack_.begin=bef=iter;
 					}
 					else
 					{
@@ -116,14 +95,6 @@ namespace nThread
 					bef=iter;
 					iter=iter->next;
 				}
-		}
-		//1. if constructor or assignment operator you use here is not noexcept, it may not be exception safety
-		//2. will block wait_and_pop_front
-		value_type wait_and_pop_front()
-		{
-			std::unique_lock<std::mutex> lock{wait_mut_};
-			cv_.wait(lock,[this]() noexcept{return !empty();});
-			return pop_front_();	//is pop_front_ instead of pop_front
 		}
 		CThread_forward_list& operator=(const CThread_forward_list &)=delete;
 	};
