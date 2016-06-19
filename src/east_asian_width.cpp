fca9742a71c2a8df98cc1cfe1499c08469ef0fe0
Fdhvdu <fdhvducipher@gmail.com>
2016-06-19 09:09:03 +0800

    update (error message)

--- src/east_asian_width.cpp
+++ src/east_asian_width.cpp
@@ -1,5 +1,6 @@
 #include"../header/tool/east_asian_width.hpp"
 #include<fstream>
+#include<iostream>
 using namespace std;
 
 namespace
@@ -7,11 +8,16 @@ namespace
 	unordered_map<unsigned long,char> read_east_asian_width_property()
 	{
 		ifstream ifs{"east_asian_width_property",ios_base::binary|ios_base::in};
-		unordered_map<unsigned long,char> east_asian_width_property{0x110000};
-		char c;
-		for(unsigned long i{0};ifs>>c;++i)
-			east_asian_width_property[i]=c;
-		return east_asian_width_property;
+		if(ifs.is_open())
+		{
+			unordered_map<unsigned long,char> east_asian_width_property{0x110000};
+			char c;
+			for(unsigned long i{0};ifs>>c;++i)
+				east_asian_width_property[i]=c;
+			return east_asian_width_property;
+		}
+		cerr<<"east_asian_width_property does not exist"<<endl;
+		return {};
 	}
 }
 
