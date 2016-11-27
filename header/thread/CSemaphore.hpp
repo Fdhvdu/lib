a4cf0a9d614f4f84ca812bdc9006a5e2ef12746b
Fdhvdu <fdhvducipher@gmail.com>
2016-11-27 12:05:10 +0800

    add sub_if_greater_than_0

--- header/thread/CSemaphore.hpp
+++ header/thread/CSemaphore.hpp
@@ -1,32 +1,108 @@
 #ifndef CSEMAPHORE
 #define CSEMAPHORE
 #include<cstddef>
-#include"../tool/CPimpl.hpp"
+#include<type_traits>
+#include"../tool/CUnique_obj.hpp"
 
 namespace nThread
 {
-	class CSemaphore
+	//this namespace is not for user
+	namespace nImpl
+	{
+		class CSemaphore
+		{
+		public:
+			using size_type=std::size_t;
+		private:
+			struct Impl;
+			nTool::CUnique_obj<Impl> impl_;
+		public:
+			explicit CSemaphore(size_type);
+			CSemaphore(const CSemaphore &)=delete;
+			size_type count() const noexcept;
+			void reset() noexcept;
+			void signal();
+			bool sub_if_greater_than_0() noexcept;
+			void wait();
+			void wait_with_sub();
+			CSemaphore& operator=(const CSemaphore &)=delete;
+			~CSemaphore();
+		};
+	}
+	
+	struct Use_sub_if_greater_than_0{};
+	struct Do_not_use_sub_if_greater_than_0{};
+
+	template<class UseSubIfGreaterThan0>
+	class CBasic_semaphore
 	{
 	public:
-		using size_type=std::size_t;
+		using size_type=nImpl::CSemaphore::size_type;
+		enum Policy:bool
+		{
+			USE_SUB_IF_GREATER_THAN_0=std::is_same<UseSubIfGreaterThan0,Use_sub_if_greater_than_0>::value
+		};
 	private:
-		struct Impl;
-		nTool::CPimpl<Impl> impl_;
+		struct Check_type
+		{
+			std::enable_if_t<std::is_same<UseSubIfGreaterThan0,Use_sub_if_greater_than_0>::value||std::is_same<UseSubIfGreaterThan0,Do_not_use_sub_if_greater_than_0>::value> UseSubIfGreaterThan0_is_correct;
+		};
+		struct Wait
+		{
+			static void wait(nImpl::CSemaphore &sema)
+			{
+				sema.wait();
+			}
+		};
+		struct Wait_with_sub
+		{
+			static void wait(nImpl::CSemaphore &sema)
+			{
+				sema.wait_with_sub();
+			}
+		};
+		nImpl::CSemaphore sema_;
 	public:
-		//CSemaphore::CSemaphore()
-		//	:CSemaphore{0}{}
-		CSemaphore();
-		explicit CSemaphore(size_type);
-		CSemaphore(const CSemaphore &)=delete;
-		size_type count() const noexcept;
-		//1. make CSemaphore::count become 0
+		CBasic_semaphore()
+			:CBasic_semaphore{0}{}
+		explicit CBasic_semaphore(const size_type count)
+			:sema_{count}{}
+		CBasic_semaphore(const CBasic_semaphore &)=delete;
+		inline size_type count() const noexcept
+		{
+			return sema_.count();
+		}
+		//1. make count() become 0
 		//2. will not signal any threads
-		void reset();
-		void signal();
-		void wait();
-		CSemaphore& operator=(const CSemaphore &)=delete;
-		~CSemaphore();
+		inline void reset() noexcept
+		{
+			sema_.reset();
+		}
+		//count() plus 1 and notify waiting threads
+		inline void signal()
+		{
+			sema_.signal();
+		}
+		//1. count() minus 1 if count() is greater than 0
+		//2. do not wait
+		//3. return true if count() is greater than 0 before minus; otherwise, return false
+		template<class=std::enable_if_t<USE_SUB_IF_GREATER_THAN_0>>
+		inline bool sub_if_greater_than_0() noexcept
+		{
+			return sema_.sub_if_greater_than_0();
+		}
+		//waiting until notifying if count() is 0; otherwise, count() minus 1
+		inline void wait()
+		{
+			std::conditional_t<USE_SUB_IF_GREATER_THAN_0,Wait_with_sub,Wait>::wait(sema_);	//for C++17, use if constexpr
+		}
+		CBasic_semaphore& operator=(const CBasic_semaphore &)=delete;
 	};
+
+	//notice, the definition of CBasic_semaphore will be changed in the future
+	//but the name (CSemaphore and CSemaphore_with_sub) and tag (Do_not_use_sub_if_greater_than_0 and Use_sub_if_greater_than_0) will not be changed in the future
+	using CSemaphore=CBasic_semaphore<Do_not_use_sub_if_greater_than_0>;
+	using CSemaphore_with_sub=CBasic_semaphore<Use_sub_if_greater_than_0>;
 }
 
 #endif
--- src/CSemaphore.cpp
+++ src/CSemaphore.cpp
@@ -6,62 +6,91 @@ using namespace std;
 
 namespace nThread
 {
-	struct CSemaphore::Impl
+	namespace nImpl
 	{
-		atomic<CSemaphore::size_type> count;
-		condition_variable cv;
-		atomic_flag only_one_notify;
-		mutex mut;
-		Impl(CSemaphore::size_type);
-		void signal();
-		void wait();
-	};
-
-	CSemaphore::Impl::Impl(const CSemaphore::size_type count_)
-		:count{count_},only_one_notify{ATOMIC_FLAG_INIT}{}
-
-	void CSemaphore::Impl::signal()
-	{
-		if(!count++&&!only_one_notify.test_and_set(std::memory_order_acquire))
+		struct CSemaphore::Impl
 		{
-			lock_guard<mutex> lock{mut};
-			cv.notify_all();
-			only_one_notify.clear(std::memory_order_release);
+			atomic<CSemaphore::size_type> count;
+			condition_variable cv;
+			atomic_flag only_one_notify;
+			mutex mut;
+			Impl(CSemaphore::size_type);
+			void signal();
+			bool sub_if_greater_than_0() noexcept;
+			void wait();
+			void wait_with_sub();
+		};
+
+		CSemaphore::Impl::Impl(const CSemaphore::size_type count_)
+			:count{count_},only_one_notify{ATOMIC_FLAG_INIT}{}
+
+		void CSemaphore::Impl::signal()
+		{
+			if(!count++&&!only_one_notify.test_and_set(std::memory_order_acquire))
+			{
+				lock_guard<mutex> lock{mut};
+				cv.notify_all();
+				only_one_notify.clear(std::memory_order_release);
+			}
 		}
-	}
 
-	void CSemaphore::Impl::wait()
-	{
-		unique_lock<mutex> lock{mut};
-		cv.wait(lock,[this]() noexcept{return count.operator CSemaphore::size_type();});
-		--count;
-	}
+		bool CSemaphore::Impl::sub_if_greater_than_0() noexcept
+		{
+			auto val{count.load(std::memory_order_relaxed)};
+			do
+			{
+				if(!val)
+					return false;
+			}while(!count.compare_exchange_weak(val,val-1,std::memory_order_acquire,std::memory_order_relaxed));
+			return true;
+		}
 
-	CSemaphore::CSemaphore()
-		:CSemaphore{0}{}
+		void CSemaphore::Impl::wait()
+		{
+			unique_lock<mutex> lock{mut};
+			cv.wait(lock,[this]() noexcept{return count.operator CSemaphore::size_type();});
+			--count;
+		}
 
-	CSemaphore::CSemaphore(const CSemaphore::size_type count)
-		:impl_{count}{}
+		void CSemaphore::Impl::wait_with_sub()
+		{
+			unique_lock<mutex> lock{mut};
+			cv.wait(lock,[this]() noexcept{return sub_if_greater_than_0();});
+		}
 
-	CSemaphore::size_type CSemaphore::count() const noexcept
-	{
-		return impl_.get().count;
-	}
+		CSemaphore::CSemaphore(const CSemaphore::size_type count)
+			:impl_{count}{}
 
-	void CSemaphore::reset()
-	{
-		impl_.get().count=0;
-	}
+		CSemaphore::size_type CSemaphore::count() const noexcept
+		{
+			return impl_->count;
+		}
 
-	void CSemaphore::signal()
-	{
-		impl_.get().signal();
-	}
+		void CSemaphore::reset() noexcept
+		{
+			impl_->count=0;
+		}
 
-	void CSemaphore::wait()
-	{
-		impl_.get().wait();
-	}
+		void CSemaphore::signal()
+		{
+			impl_->signal();
+		}
 
-	CSemaphore::~CSemaphore()=default;
+		bool CSemaphore::sub_if_greater_than_0() noexcept
+		{
+			return impl_->sub_if_greater_than_0();
+		}
+
+		void CSemaphore::wait()
+		{
+			impl_->wait();
+		}
+
+		void CSemaphore::wait_with_sub()
+		{
+			impl_->wait_with_sub();
+		}
+
+		CSemaphore::~CSemaphore()=default;
+	}	
 }
