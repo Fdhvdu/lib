f06c50b59553ed9a78f61b0564d4ec642f1750e6
Fdhvdu <fdhvducipher@gmail.com>
2016-12-07 15:08:56 +0800

    make it template (so that I can change mutex)

--- header/thread/CSemaphore.hpp
+++ header/thread/CSemaphore.hpp
@@ -1,105 +1,89 @@
 #ifndef CSEMAPHORE
 #define CSEMAPHORE
+#include<atomic>
+#include<condition_variable>
 #include<cstddef>
+#include<mutex>
 #include<type_traits>
-#include"../tool/CUnique_obj.hpp"
 
 namespace nThread
 {
-	//this namespace is not for user
-	namespace nImpl
-	{
-		class CSemaphore
-		{
-		public:
-			using size_type=std::size_t;
-		private:
-			struct Impl;
-			nTool::CUnique_obj<Impl> impl_;
-		public:
-			explicit CSemaphore(size_type);
-			CSemaphore(const CSemaphore &)=delete;
-			size_type count() const noexcept;
-			void reset() noexcept;
-			void signal();
-			bool sub_if_greater_than_0() noexcept;
-			void wait();
-			void wait_with_sub();
-			CSemaphore& operator=(const CSemaphore &)=delete;
-			~CSemaphore();
-		};
-	}
-	
 	struct Use_sub_if_greater_than_0{};
 	struct Do_not_use_sub_if_greater_than_0{};
 
-	template<class UseSubIfGreaterThan0>
+	template<class SubIfGreaterThan0,class Mutex>
 	class CBasic_semaphore
 	{
 	public:
-		using size_type=nImpl::CSemaphore::size_type;
-		enum Policy:bool
-		{
-			USE_SUB_IF_GREATER_THAN_0=std::is_same<UseSubIfGreaterThan0,Use_sub_if_greater_than_0>::value
-		};
+		using mutex_type=Mutex;
+		using size_type=std::size_t;
+		static constexpr bool SUB_IF_GREATER_THAN_0{std::is_same<SubIfGreaterThan0,Use_sub_if_greater_than_0>::value};
 	private:
-		using check_UseSubIfGreaterThan0_type=std::enable_if_t<std::is_same<UseSubIfGreaterThan0,Use_sub_if_greater_than_0>::value||std::is_same<UseSubIfGreaterThan0,Do_not_use_sub_if_greater_than_0>::value>;
-		struct Wait
+		using check_SubIfGreaterThan0_type=std::enable_if_t<std::is_same<SubIfGreaterThan0,Use_sub_if_greater_than_0>::value||std::is_same<SubIfGreaterThan0,Do_not_use_sub_if_greater_than_0>::value>;
+		std::atomic<size_type> count_;
+		std::conditional_t<std::is_same<mutex_type,std::mutex>::value,std::condition_variable,std::condition_variable_any> cv_;
+		std::atomic_flag only_one_notify_;
+		mutex_type mut_;
+		void wait_(std::true_type)
 		{
-			static void wait(nImpl::CSemaphore &sema)
-			{
-				sema.wait();
-			}
-		};
-		struct Wait_with_sub
+			std::unique_lock<mutex_type> lock{mut_};
+			cv_.wait(lock,[this]() noexcept{return sub_if_greater_than_0();});
+		}
+		void wait_(std::false_type)
 		{
-			static void wait(nImpl::CSemaphore &sema)
-			{
-				sema.wait_with_sub();
-			}
-		};
-		nImpl::CSemaphore sema_;
+			std::unique_lock<mutex_type> lock{mut_};
+			cv_.wait(lock,[this]() noexcept{return count();});
+			--count_;
+		}
 	public:
 		CBasic_semaphore()
 			:CBasic_semaphore{0}{}
 		explicit CBasic_semaphore(const size_type count)
-			:sema_{count}{}
+			:count_{count},only_one_notify_{ATOMIC_FLAG_INIT}{}
 		CBasic_semaphore(const CBasic_semaphore &)=delete;
 		inline size_type count() const noexcept
 		{
-			return sema_.count();
+			return count_;
 		}
 		//1. make count() become 0
 		//2. will not signal any threads
 		inline void reset() noexcept
 		{
-			sema_.reset();
+			count_.store(0);
 		}
 		//count() plus 1 and notify waiting threads
-		inline void signal()
+		void signal()
 		{
-			sema_.signal();
+			if(!count_++&&!only_one_notify_.test_and_set(std::memory_order_acquire))
+			{
+				std::lock_guard<mutex_type> lock{mut_};
+				cv_.notify_all();
+				only_one_notify_.clear(std::memory_order_release);
+			}
 		}
 		//1. count() minus 1 if count() is greater than 0
 		//2. do not wait
 		//3. return true if count() is greater than 0 before minus; otherwise, return false
-		inline bool sub_if_greater_than_0() noexcept
+		bool sub_if_greater_than_0() noexcept
 		{
-			using is_enable=std::enable_if_t<USE_SUB_IF_GREATER_THAN_0>;
-			return sema_.sub_if_greater_than_0();
+			using is_enable=std::enable_if_t<SUB_IF_GREATER_THAN_0>;
+			auto val{count_.load(std::memory_order_relaxed)};
+			do
+			{
+				if(!val)
+					return false;
+			}while(!count_.compare_exchange_weak(val,val-1,std::memory_order_acquire,std::memory_order_relaxed));
+			return true;
 		}
 		//waiting until notifying if count() is 0; otherwise, count() minus 1
 		inline void wait()
 		{
-			std::conditional_t<USE_SUB_IF_GREATER_THAN_0,Wait_with_sub,Wait>::wait(sema_);	//for C++17, use if constexpr
+			wait_(std::is_same<SubIfGreaterThan0,Use_sub_if_greater_than_0>{});
 		}
 		CBasic_semaphore& operator=(const CBasic_semaphore &)=delete;
 	};
 
-	//notice, the definition of CBasic_semaphore will be changed in the future
-	//but the name (CSemaphore and CSemaphore_with_sub) and tag (Do_not_use_sub_if_greater_than_0 and Use_sub_if_greater_than_0) will not be changed in the future
-	using CSemaphore=CBasic_semaphore<Do_not_use_sub_if_greater_than_0>;
-	using CSemaphore_with_sub=CBasic_semaphore<Use_sub_if_greater_than_0>;
+	using CSemaphore=CBasic_semaphore<Do_not_use_sub_if_greater_than_0,std::mutex>;
 }
 
 #endif
