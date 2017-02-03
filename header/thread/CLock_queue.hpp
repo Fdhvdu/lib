738b4fbfacee9ee36c8a63a91fcb1375b5b38c68
Fdhvdu <fdhvducipher@gmail.com>
2017-02-03 19:25:00 +0800

    fix

--- header/thread/CLock_queue.hpp
+++ header/thread/CLock_queue.hpp
@@ -1,12 +1,13 @@
 #ifndef CLOCK_QUEUE
 #define CLOCK_QUEUE
+#include<mutex>
 #include"CBasic_ts_container.hpp"
 #include"Lock_queue.hpp"
 
 namespace nThread
 {
 	template<class T>
-	using CLock_queue=CBasic_ts_container<Lock_queue<T>>;
+	using CLock_queue=CBasic_ts_container<Lock_queue<T,std::mutex>>;
 }
 
 #endif
