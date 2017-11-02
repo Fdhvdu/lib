4b158aad77c79a38a716907d4109ed62ed3e3f6b
Fdhvdu <fdhvducipher@gmail.com>
2017-11-02 11:45:08 +0800

    add CSignal_guard

---
+++ header/tool/CSignal_guard.hpp
@@ -0,0 +1,19 @@
+#ifndef CSIGNAL_GUARD
+#define CSIGNAL_GUARD
+#include"CUnique_obj.hpp"
+
+namespace nTool
+{
+	class CSignal_guard
+	{
+		struct Impl;
+		CUnique_obj<Impl> impl_;
+	public:
+		CSignal_guard();
+		bool alive() const noexcept;
+		void complete();
+		~CSignal_guard();
+	};
+}
+
+#endif
