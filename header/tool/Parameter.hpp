18162e1dd354ca2a51482fb67b27ae74595477e0
Fdhvdu <fdhvducipher@gmail.com>
2018-04-28 15:42:06 +0800

    fix compilation error when two types equal

--- header/tool/Parameter.hpp
+++ header/tool/Parameter.hpp
@@ -11,23 +11,30 @@ namespace nTool
 	class Parameter
 	{
 		std::unordered_map<std::string,std::any> param_;
-	public:
 		template<class T1,class T2>
-		bool get(const std::string &name,T2 &val) const
+		bool get_(const std::string &name,T2 &val) const
 		{
 			const auto iter(param_.find(name));
 			if(iter!=std::end(param_))
 			{
-				if(const auto &temp(iter->second);temp.has_value())
-					val=std::any_cast<T1>(temp);
+				if(iter->second.has_value())
+					val=std::any_cast<T1>(iter->second);
 				return true;
 			}
 			return false;
 		}
+	public:
+		template<class T1,class T2
+			,class=std::enable_if_t<std::negation_v<std::is_same<std::remove_reference_t<T1>,T2>>>
+		>
+		bool get(const std::string &name,T2 &val) const
+		{
+			return get_<T1>(name,val);
+		}
 		template<class T>
 		inline bool get(const std::string &name,T &val) const
 		{
-			return get<T,T>(name,val);
+			return get_<T,T>(name,val);
 		}
 		inline void set(const std::string &name)
 		{
