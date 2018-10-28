f8f245fb462e897734b38cc7fd0727a40ffcaa31
Fdhvdu <fdhvducipher@gmail.com>
2018-10-28 15:19:56 +0800

    remove CChrono_timer, and add how_long

---
+++ header/tool/how_long.hpp
@@ -0,0 +1,19 @@
+#ifndef HOW_LONG_HPP
+#define HOW_LONG_HPP
+#include<chrono>
+#include<functional>
+#include<utility>	//forward
+
+namespace nTool
+{
+	template<class Func,class ... Args>
+	std::chrono::steady_clock::time_point how_long(Func &&func,Args &&...args)
+	{
+		using namespace std;
+		const auto timer_begin(chrono::steady_clock::now());
+		invoke(forward<decltype(func)>(func),forward<decltype(args)>(args)...);
+		return chrono::steady_clock::now()-timer_begin;
+	}
+}
+
+#endif
